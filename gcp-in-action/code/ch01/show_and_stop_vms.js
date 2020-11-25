054481053b0e3fa939bba62b0a88fb6995bc6712
Carl Hinderer <carl.hinderer4@gmail.com>
2020-11-24 21:41:33 -0700

    Added script for listing and stopping all vms in a project.

---
+++ gcp-in-action/code/ch01/show_and_stop_vms.js
@@ -0,0 +1,19 @@
+const gce = require('@google-cloud/compute') ({  
+  projectId: 'our-audio-282001'
+});
+
+const zone = gce.zone('us-central1-a');
+
+console.log('Getting your VMs...');
+
+zone.getVMs().then((data) => {  
+  data[0].forEach((vm) => {
+    console.log('Found a VM called', vm.name);
+    console.log('Stopping', vm.name, '...');
+    vm.stop((err, operation) => {
+      operation.on('complete', (err) => {
+        console.log('Stopped', vm.name);
+      });    
+    });
+  });
+});
